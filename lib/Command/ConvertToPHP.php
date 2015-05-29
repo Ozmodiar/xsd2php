b7ec912aede7506fffbcd340995bb99ab218de65
Joeri van Dooren <jvd@anaxis.be>
2015-05-29 09:42:33 +0200

    Fix incorrect merge result.

--- lib/Command/ConvertToPHP.php
+++ lib/Command/ConvertToPHP.php
@@ -7,7 +7,6 @@ use Goetas\Xsd\XsdToPhp\Php\PathGenerator\Psr4PathGenerator;
 use Goetas\Xsd\XsdToPhp\AbstractConverter;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Zend\Code\Generator\FileGenerator;
 use Goetas\Xsd\XsdToPhp\Naming\NamingStrategy;
